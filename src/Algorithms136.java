56745501f2ca01c337ae2dd6f32997b7b0c2e105
fucyier <canerdemir@gmail.com>
2022-12-27 23:31:47 +0300

    leetcode  config

---
+++ src/Algorithms136.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Algorithms136 {
+    public static int singleNumber(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int value = 0;
+        if (nums.length > 0) value = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != value) value = nums[i];
+        }
+
+        return value;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+
+    }
+}
