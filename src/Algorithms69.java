3e85748ad8d523a5cec4189390831dcc26a56a02
fucyier <canerdemir2@gmail.com>
2022-12-28 23:24:15 +0300

    leetcode  config

---
+++ src/Algorithms69.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Algorithms69 {
+    public static int mySqrt(int x) {
+        long i;
+        for (i = 0; i * i <= x; i++)
+            if (x == i * i || x < (i + 1) * (i + 1)) break;
+        return (int) i;
+    }
+
+    public static void main(String[] args) {
+        //int a=46341*46341;
+        System.out.println(mySqrt(2147483647));
+    }
+}
